e80391b138dd751dcf20575cc4800c98b468ef45
davidgfb <davidgracia99@gmail.com>
2026-02-20 10:48:05 +0100

    Refactor version variable handling in enums.py

--- ScriptHook/enums.py
+++ ScriptHook/enums.py
@@ -1,17 +1,14 @@
+from json import load
+
 # GTAIV.exe (WinAPI GetFileVersionInfo, hash_a_v
-vv = {
-    'UnknownVersion': 0,
-    1000: 0x1000000,
-    1010: 0x1000100,
-    1020: 0x1000200,
-    1030: 0x1000300,
-    1040: 0x1000400,
-    1050: 0x1000004,
-    1060: 0x1000600,
-    1070: 0x1000700,
-    1110: 0x1010100, # EFLC
-    1120: 0x1010200, # EFLC
-}
+
+vv = None
 
 # PROBADOR
-[print({v: hex(h)}) for v, h in vv.items()]
+try:
+    #gestiona close
+    with open('obj/vv.json', 'r') as f: vv = load(f)
+
+except Exception as e: print(f'e: {e}') # NO especifico. 4 casos
+
+[print({f'{v}: {hex(h)}'}) for v, h in vv.items()]
