5d09eeee542ad65a4145611f61841d26f7215a64
davidgfb <davidgracia99@gmail.com>
2026-02-20 10:25:38 +0100

    Add Exportador vv json.py for version exporting

---
+++ ScriptHook/Exportadores/Exportador vv json.py
@@ -0,0 +1,25 @@
+from json import dump
+
+# GTAIV.exe (WinAPI GetFileVersionInfo, hash_a_v
+vv = {
+    'UnknownVersion': 0,
+    1000: 0x1000000,
+    1010: 0x1000100,
+    1020: 0x1000200,
+    1030: 0x1000300,
+    1040: 0x1000400,
+    1050: 0x1000004,
+    1060: 0x1000600,
+    1070: 0x1000700,
+    1110: 0x1010100, # EFLC
+    1120: 0x1010200, # EFLC
+}
+
+# PROBADOR
+NOMBRE_JSON = 'vv.json'
+with open(NOMBRE_JSON, 'w') as f:\
+     dump({v: h for v, h in vv.items()}, f, indent=4)
+
+print(f'{NOMBRE_JSON} exportado con éxito.')
+
+#[print({v: hex(h)}) for v, h in vv.items()]
