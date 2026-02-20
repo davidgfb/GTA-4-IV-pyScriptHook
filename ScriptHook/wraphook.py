4d763ba782d08bab3b17e109cabe4fd41c27bef7
davidgfb <davidgracia99@gmail.com>
2026-02-20 14:30:36 +0100

    Refactor version retrieval and output formatting
    Refactor version extraction and formatting in wraphook.py.

--- ScriptHook/wraphook.py
+++ ScriptHook/wraphook.py
@@ -5,12 +5,19 @@ ms_file_version,ls_file_version=[
     GetFileVersionInfo('E:/Grand Theft Auto IV/GTAIV.exe',"\\")[e]
     for e in([PLAN % e for e in ('M','L')]
 )]
-vv = [str(f(v)) for v in (
+vv_num = [f(v) for v in (
     ms_file_version,
     ls_file_version
 ) for f in (
     HIWORD,
     LOWORD
 )]
+major, minor, patch, build = vv_num
 
-print(".".join(vv), '->', "".join(vv)) 
+vv_str = list(map(str, vv_num))
+
+hash_v = int.from_bytes(vv_num, 'big')
+
+print(f'{".".join(vv_str)} -> {"".join(vv_str)}\n\
+s/ build: {hex(hash_v & 0xFFFFFF00)} (\
+c/ build: {hex(hash_v)})') # ID_hash_v
